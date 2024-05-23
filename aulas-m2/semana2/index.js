d10be029eb45a145566cc41257da36c5bc1645c6
vitorpicolotto <picolottovitor@gmail.com>
2024-05-22 22:03:47 -0300

    Aula 2, semana 2

--- aulas-m2/semana2/index.js
+++ aulas-m2/semana2/index.js
@@ -1,3 +1,5 @@
+        //AULA 1//
+
 //Arrays em JS
 let filmes = ['Rambo', 'Braddock', 'O Grande Dragão Branco']
 let numeros = new Array (1,2,3,4,5)
@@ -79,3 +81,154 @@ console.log(filmesOrdenados)
 //Reverse - inverte o array e retorna o próprio array
 let numerosInvertidos = numeros.reverse()
 console.log(numerosInvertidos)
+
+
+
+        //AULA 2//
+
+
+//Declaração de objetos
+
+let pessoa = {
+    nome: "Vitor",
+    idade: 29,
+    profissao: "Oceanógrafo"
+}
+
+//Modificando propriedades de objetos
+
+//Acessando
+console.log(pessoa.nome)
+console.log(pessoa["nome"])
+
+//Alterando
+pessoa.profissao = "Desenvolvedor"
+console.log(pessoa.profissao)
+
+//Adicionando
+pessoa.time = "Coritiba"
+console.log(pessoa.time)
+
+//Deletar
+delete pessoa.time
+
+
+    //Chaves dinâmicas 
+
+//Criando objetos com CD
+let chaves = ["A", "B", "C"]
+let valores = [1, 2, 3]
+
+let novoArray = chaves.map((chave, indice) =>{
+    return {[chave]: valores[indice]}
+})
+console.log(novoArray)
+
+let propriedade = "nome"
+let pessoa1 = {
+    [propriedade]: "Vitor"
+}
+console.log(pessoa1)
+
+//Acessando valores de propriedades com CD
+let chave = "idade"
+let pessoa2 = {
+    nome: "VP",
+    idade: 29
+}
+console.log(pessoa2[chave])
+
+//Criando propriedades dinamicamente / propriedades dinamicas
+let usuario = {}
+let chave1 = "email"
+
+usuario[chave1] = "vitor@email.com"
+console.log(usuario)
+
+//Usando expressões como chaves
+let contador = 0;
+let prefixo = "item"
+let carrinho ={
+    [prefixo + ++contador]: "Livros",
+    [prefixo + ++contador]: "Caneta",
+    [prefixo + ++contador]: "Borracha"
+}
+
+console.log(carrinho)
+
+//Iterando arrays para criar propriedades
+let campos = ["nome", "idade", "email"]
+let valores1 = ["Vitor", 28, "vitor@email.com"]
+let usuario1 = {}
+
+campos.forEach((campo, indice) =>{
+   usuario1[campo] = valores1[indice]
+})
+console.log(usuario1)
+
+    //Hash Map
+
+let mapa = new Map()
+mapa.set("nome", "André") //set para adicionar
+mapa.set("idade", 32) 
+mapa.set("profissão", "jornalista")
+
+console.log(mapa.get("nome")) //pega a chave
+console.log(mapa.has("idade")) //tem essa chave?
+
+mapa.delete("profissão")
+console.log(mapa.has("profissão")) 
+
+mapa.forEach((valor, chave) =>{
+    console.log(`${chave}: ${valor}`)
+})
+
+
+    //Classe Object
+//Base de todos os objetos em JS. Todos os objetos herdam métodos e propriedades da classe Object.
+
+console.log(Object.keys(pessoa))
+console.log(Object.values(pessoa))
+console.log(Object.entries(pessoa))
+
+//Object.assign - Junta objetos diferentes dentro de um objeto
+const dados = Object.keys(pessoa).map((p) =>{
+    return {[p]: pessoa[p]}
+})
+const novoObjeto = Object.assign({}, ...dados)
+
+console.log(novoObjeto)
+
+//Object.freeze - impede uma alteração no objeto
+let livro = {titulo: "Harry Potter e a Ordem da Fênix"}
+Object.freeze(livro)
+livro.titulo = "Senhor dos Anés: A Sociedade do Anel"
+console.log(livro.titulo) //não altera o valor do objeto
+
+    //Integração Array, Objeto e Hash Map
+
+let paresChaveValor = [
+    ["nome", "André"],
+    ["idade", 32],
+    ["profissão", "jornalista"]
+]
+let mapa1 = new Map(paresChaveValor)
+console.log(mapa1.get("nome"))
+
+
+let dados1 = [
+    {id: 1, nome: "André", notas: [7, 9, 8]},
+    {id: 2, nome: "Maria", notas: [6, 7, 8]},
+    {id: 3, nome: "João", notas: [8, 9, 8]}
+]
+
+let medias = dados1.map(({nome, notas})=>{ //aqui desestruturou o objeto pra já chamar nome e nota
+    let soma = notas.reduce((total, nota) =>{ //reduce para acumular
+        return total + nota
+    }, 0)
+    let media = soma/notas.length
+
+    return {nome, media}
+})
+
+console.log(medias)
